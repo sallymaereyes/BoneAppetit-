950113e48514df06c89d5002d6cd7a4985bbc1b7
Sallymae Reyes <sallymaaee@gmail.com>
2017-11-19 16:07:29 -0800

    Added DestoryOnContact Script where when the bone is caught in the bowl, it disappears

---
+++ Assets/Scripts/DestoryOnContact.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other)
+    {
+        Destroy (other.gameObject);
+
+    }
+}
